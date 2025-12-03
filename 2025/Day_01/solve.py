ce4b75a8a2966c35337a4ca0bfa0c4f7a056e8dd
Tezahc <tezahc23@gmail.com>
2025-12-03 15:50:34 +0100

    refacto en fonction

--- 2025/Day_01/solve.py
+++ 2025/Day_01/solve.py
@@ -8,18 +8,34 @@ dial = 50
 logging.info(f"Dial : {dial}")
 values = Counter()
 
-with Path("./inputfile.txt").open("r") as f:
-    for line in f:
-        direction = -1 if line[0] == 'L' else 1
-        try:
-            value = int(line[1:])
-        except ValueError as ve:
-            logging.error(f"{line[1:]} n'est pas un nombre ; {ve}")
-        except Exception as e:
-            logging.critical(f"Innatendu : {e}")
+def get_dial_changes(filepath:str | Path):
+    filepath = Path(filepath)
+    if not filepath.exists():
+        logging.critical(f"Le chemin `{filepath}` n'existe pas !")
+        raise ValueError(f"Chemin incorrect. Interruption.")
+    
+    with Path(filepath).open("r") as f:
+        for line in f:
+            direction = -1 if line[0] == 'L' else 1 # else = line[0] == 'R'
+            try:
+                value = int(line[1:])
+            except ValueError as ve:
+                logging.error(f"{line[1:]} n'est pas un nombre ; {ve}")
+            except Exception as e:
+                logging.critical(f"Innatendu : {e}")
+            
+            yield direction, value
 
-        change = direction * value
-        logging.debug(f"value : {value}, direction : {direction}, product : {change}")
+
+logging.info(f"Partie 1 - Dial : {dial}")
+def part_1(dial_start = 50):
+    dial = dial_start
+    # init du générateur qui lit chaque ligne du fichier
+    lines = get_dial_changes('inputfile.txt')
+
+    for dir, val in lines:
+        change = dir * val
+        logging.debug(f"value : {val}, direction : {dir}, product : {change}")
 
         dial += change
         logging.debug(f"dial + change : {dial}")
