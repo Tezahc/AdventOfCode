78901882dc95562ae1168c3cfca57e36ad747c8d
Tezahc <tezahc23@gmail.com>
2025-12-03 20:35:57 +0100

    cleanup & renaming

--- 2025/Day_03/solve.py
+++ 2025/Day_03/solve.py
@@ -8,21 +8,29 @@ def get_file_input(file:str | Path = 'inputfile.txt'):
         for line in f:
             yield line.strip()
 
-def find_max_joltage(jolt_values:str, jolt_len:int = 2, acc:list=None) -> int:
+def digit_lookup(digits:str, start:int=None, end:int=None) -> str:
+    digit = max(int(i) for i in digits[start:end])
+    return str(digit)
+
+def find_max_joltage(jolt_values:str, batt_len:int = 2, acc:list = None) -> int:
     if acc is None: acc = []
-    logging.debug(f"début fonction : {jolt_values}, {jolt_len}, {acc}")
+    logging.debug(f"début fonction : {jolt_values}, {batt_len}, {acc}")
+
     # condition de fin : dernier chiffre à trouvé
-    if jolt_len - 1 == 0:
-        acc.append(str(max(int(i) for i in jolt_values)))
+    if batt_len - 1 == 0:
+        acc.append(digit_lookup(jolt_values))
         # renvoie le nombre assemblé
         logging.debug(f"retour de fontion : {acc}")
+
         return int(''.join(acc))
     
-    nth_digit = max(int(i) for i in jolt_values[:-(jolt_len-1)])
-    nth_digit_position = jolt_values.find(str(nth_digit))
-    acc.append(str(nth_digit))
+    nth_digit = digit_lookup(jolt_values, end=-(batt_len-1) )
+    nth_digit_position = jolt_values.find(nth_digit)
+    acc.append(nth_digit)
     logging.debug(f"avant appel récursif : digit={nth_digit} - pos={nth_digit_position} - cumul={acc}")
-    return find_max_joltage(jolt_values[nth_digit_position+1:], jolt_len - 1, acc)
+
+    return find_max_joltage(jolt_values[nth_digit_position+1:], batt_len-1, acc)
+
 
 # === PARTIE 1 ===
 jolts = get_file_input()
