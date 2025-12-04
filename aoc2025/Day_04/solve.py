3877e96dbce5ffeee47d0c79d0cc9863086c1144
Tezahc <tezahc23@gmail.com>
2025-12-04 13:51:08 +0100

    Day 4 - Partie 1 - utilisation d'une étape de convolution ! comme en ML !

--- aoc2025/Day_04/solve.py
+++ aoc2025/Day_04/solve.py
@@ -1,3 +1,27 @@
 from aoc2025.utils import read_input
 import numpy as np
-import scipy
+from scipy.signal import convolve2d
+
+# === PARTIE 1 ===
+grid_txt = read_input()
+
+# conversion des caractères en nombres
+mapper={"@":1, ".":0}
+data = [
+    [mapper[c] for c in line]
+    for line in grid_txt
+]
+
+grid = np.array(data)
+kernel = np.array([
+    [1,1,1],
+    [1,0,1],
+    [1,1,1]
+])
+# applique une convolution simple pour compter le nombre de voisin d'une cell
+convo = convolve2d(grid, kernel, mode='same', boundary='fill')
+# filtre la convo avec la valeur du centre
+filter = np.where((grid==1) & (convo<4), 1, 0)
+filter.sum()
+
+print(f"Il y a {filter.sum()} rouleaux accessibles en forklift pour l'input donné.")
